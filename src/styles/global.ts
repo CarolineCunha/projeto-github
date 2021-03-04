b17175169f036e5a5226cfe164de12d98e6b4887
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-03-04 07:39:10 -0300

    conectando a api do github

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,8 +18,8 @@ export default createGlobalStyle `
     }
 
     #root {
-        max-width: 900px;
-        margin: 0px;
+        max-width: 960px;
+        margin: 0 auto;
         padding: 40px 20px;
     }
 
